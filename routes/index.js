d318777907646e66d792dd417674ac5db4318487
brigade16 <belkhiria.badreddine@gmail.com>
2014-02-13 22:54:50 +0000

    upload

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+exports.index = function (req, res) {
+    res.render('index', {waitingList: global.waitingList, last: global.last, count: global.count, timeOut: global.timeOut, running: global.running});
+};
+
+exports.stat = function (req, res) {
+    res.send(true);
+};
+
+exports.add = function (req, res) {
+    if(global.running){
+        global.waitingList.push(req.body);
+        res.send(true);
+    }
+    else
+        res.send(false);
+};
+
+exports.start = function (req, res) {
+    global.waitingList = [];
+    global.running = true;
+    res.send(true);
+};
+
+exports.stop = function (req, res) {
+    global.waitingList = [];
+    global.running = false;
+    res.send(true);
+};
---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var request = require('request');
+var lbcscraper = require('./lib/lbcscraper');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+global.running = true;
+global.last = new Date().toString('T');
+global.waitingList = [];
+global.lbcscraper = new lbcscraper();
+global.count = 0;
+
+app.get('/', routes.index);
+app.post('/stat', routes.stat);
+app.post('/add', routes.add);
+app.post('/start', routes.start);
+app.post('/stop', routes.stop);
+
+http.createServer(app).listen(app.get('port'), app.get('ip'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+    global.lbcscraper.run();
+    var requestJSON = require('request-json');
+    var client = requestJSON.newClient('http://localhost:3000/');
+    client.post('add', {options:{
+        url: "http://www.leboncoin.fr/voitures/621302665.htm?ca=12_s",
+        port: 80,
+        method: 'GET'
+    }, title:"",  particulier: true, urgent: true}, function (err, res, body) {
+        if (err) {
+            console.error(err);
+            res.send(global.running);
+        }
+        else{
+            console.log(body);
+        }
+    });
+});
